afe1a2bbc2ed891d5baf542f9ac9340385a148f7
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-07-21 18:51:12 +0530

    Create oops.1.py

---
+++ oops.1.py
@@ -0,0 +1,25 @@
+# class MyClass:
+#     pass
+#
+#
+# obj1 = MyClass()
+# obj2 = MyClass()
+#
+# print(type(obj1))
+
+# class Student:
+#     pass
+#
+# s1=Student()
+# s2=Student()
+#
+# s1.name='john'
+# s1.roll=100
+
+class Student:
+    def study(arg):
+        return "the student studies for 3 hours a day"
+
+s1=Student()
+
+print(s1.study())
