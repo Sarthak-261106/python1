ba82ae192ea654d9067975ed892250443d5285e6
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-07-25 16:13:52 +0530

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,24 @@
+class Vehicle:
+    company='xyz motors'
+
+    def __init__(self,n_wheels,n_seats,mileage):
+        self.n_wheels=n_wheels
+        self.n_seats=n_seats
+        self.mileage=mileage
+
+    def get_details(self):
+        return f'this vehicle has {self.n_wheels} wheels and {self.n_seats} seats and provide mileage of {self.mileage}'
+
+# v1=Vehicle(4,5,20)
+# print(v1.get_details())
+
+class Car(Vehicle):
+    def __init__(self,car_type,drive_type):
+        print('init of car')
+        self.car_type=car_type
+        self.drive_type=drive_type
+        super().__init__(4,5,20)
+
+c1=Car('sidan','manual')
+print(c1.get_details())
+print(c1.__dict__)
