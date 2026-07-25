4a63910ebc045a62dcf10f2df5e693826ce3a55b
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-07-25 17:08:01 +0530

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,20 @@
+#overloading
+
+
+class Rectangle:
+    def __init__(self,length,breadth):
+        self.length=length
+        self.breadth=breadth
+
+    def area(self):
+        return self.length*self.breadth
+
+    def __add__(self,other):
+        return self.length+other.length
+
+r1=Rectangle(3,4)
+r2=Rectangle(5,6)
+print(r1.area())
+print(r2.area())
+
+print(r1+r2)
