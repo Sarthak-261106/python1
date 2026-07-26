791d2057b3d0008893bc88ccd3f421e620c8037b
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-07-26 17:26:58 +0530

    Update tkinterrr.py

--- tkinterrr.py
+++ tkinterrr.py
@@ -1,13 +1,26 @@
 import tkinter as tk
 import tkinter.font as tkFont
+from tkinter import ttk
 window = tk.Tk()
 window.title("my window")
 window.minsize(400,300)
-#custom_font=tkFont.Font(family="Times New Roman",size=20,weight="bold")
+custom_font=tkFont.Font(family="Times New Roman",size=20,weight="bold")
 
-label = tk.Label(text="my label")
+label = ttk.Label(text="my label",font=custom_font)
+label.pack()
+#label.config(font=("Courier New",25,"underline"))
 
-label.pack(expand=True)
-label.config(font=("Courier New",25,"underline"))
+# counter=0
+def funtion_button():
+    # global counter
+    # counter+=1
+    input_text=user_input.get()
+    label.config(text=input_text)
+button = ttk.Button(text="click me",command=funtion_button)
+button.pack()
+
+user_input=ttk.Entry(width=20,font=custom_font)
+user_input.pack()
+
 window.mainloop()
 
