0ba4f9b73741a00508ad28b5348e308961ee8df2
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-07-26 17:30:58 +0530

    Create quitting.py

---
+++ quitting.py
@@ -0,0 +1,30 @@
+
+import tkinter as tk
+import tkinter.font as tkFont
+from tkinter import ttk
+window = tk.Tk()
+window.title("my window")
+window.minsize(400,300)
+custom_font=tkFont.Font(family="Times New Roman",size=20,weight="bold")
+
+label = tk.Label(text="my label",font=custom_font)
+label.pack()
+#label.config(font=("Courier New",25,"underline"))
+
+# counter=0
+def funtion_button():
+    # global counter
+    # counter+=1
+    input_text=user_input.get()
+    label.config(text=input_text)
+button = tk.Button(text="click me",command=funtion_button)
+button.pack()
+
+user_input=tk.Entry(width=20,font=custom_font)
+user_input.pack()
+
+quit_button=ttk.Button(text="Quit",command=window.destroy)
+quit_button.pack()
+
+window.mainloop()
+
